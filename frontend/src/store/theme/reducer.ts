110df758d3c6504f7ae80ba47642207f2105fe80
HyeonTae <lsnwan@naver.com>
2023-03-29 16:57:48 +0900

    로그 삭제

--- frontend/src/store/theme/reducer.ts
+++ frontend/src/store/theme/reducer.ts
@@ -8,7 +8,6 @@ const initialState: T.State = Utils.getCookie('theme');
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
   switch (action.type) {
     case "@theme/setTheme":
-      console.log('reducer', action.payload);
       return action.payload
   }
   return state
