1e802285e4c8e9bf2d492ed7551f7a871e34215f
HyeonTae <lsnwan@naver.com>
2023-03-29 16:59:39 +0900

    헤더 추가

--- frontend/src/pages/CommLayout.tsx
+++ frontend/src/pages/CommLayout.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import {CommonStyled} from "../components/styled/common-styled";
+import Header from "../components/cmm/Header";
+import {useSelector} from "react-redux";
+import {AppState} from "../store";
+import * as T from "../store/theme";
 
 const CommonLayout = () => {
 
+  const theme = useSelector<AppState, T.State>(state => state.themeType);
+
   return (
     <CommonStyled>
+      <Header theme={theme}/>
       <Outlet />
     </CommonStyled>
   );
