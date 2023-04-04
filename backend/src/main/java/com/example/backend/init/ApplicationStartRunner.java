697634f6c0b9e0320aebc42e1fcdbeaf32fabf91
HyeonTae <lsnwan@naver.com>
2023-04-04 20:39:19 +0900

    기본 세팅

--- backend/src/main/java/com/example/backend/init/ApplicationStartRunner.java
+++ backend/src/main/java/com/example/backend/init/ApplicationStartRunner.java
@@ -3,8 +3,10 @@ package com.example.backend.init;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("default")
 @Component
 @Slf4j
 public class ApplicationStartRunner implements ApplicationRunner {
