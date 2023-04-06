4e8b07ce2ba8c7b040b5cd95afab403b846b6205
HyeonTae <lsnwan@naver.com>
2023-04-06 19:47:34 +0900

    @Bean 추가

--- backend/src/main/java/com/example/backend/config/JasyptConfig.java
+++ backend/src/main/java/com/example/backend/config/JasyptConfig.java
@@ -3,6 +3,7 @@ package com.example.backend.config;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,6 +13,7 @@ public class JasyptConfig {
     @Value("${jasypt.password}")
     private String password;
 
+    @Bean
     public PooledPBEStringEncryptor jasyptStringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         encryptor.setPoolSize(4);
