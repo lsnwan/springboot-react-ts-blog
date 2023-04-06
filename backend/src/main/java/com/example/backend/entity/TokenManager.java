eaf3cf091733a1e335440c274bfa8112958b2d47
HyeonTae <lsnwan@naver.com>
2023-04-06 19:49:34 +0900

    토큰 관리 엔티티 추가

---
+++ backend/src/main/java/com/example/backend/entity/TokenManager.java
@@ -0,0 +1,29 @@
+package com.example.backend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "token_manager")
+@Getter @Setter @EqualsAndHashCode(of = "idx")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenManager {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tm_idx")
+    private Long idx;
+
+    @Column(name = "acc_id")
+    private String accountId;
+
+    @Column(name = "tm_refsh_tokn")
+    private String refreshToken;
+
+    @Column(name = "tm_tokn_expire")
+    private LocalDateTime expireTime;
+
+}
