b37475c9222a527239cc3dbd0081b2c1561d1090
HyeonTae <lsnwan@naver.com>
2023-04-09 17:14:48 +0900

    권한 조회 쿼리 추가

--- backend/src/main/java/com/example/backend/repository/AuthorityRepository.java
+++ backend/src/main/java/com/example/backend/repository/AuthorityRepository.java
@@ -3,5 +3,9 @@ package com.example.backend.repository;
 import com.example.backend.entity.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+
+    List<Authority> findAllByOrderByAuthSort();
 }
