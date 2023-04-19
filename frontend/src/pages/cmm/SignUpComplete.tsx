5e02af2d6ea18d48ca973a6c4f0eb01216d20ddc
HyeonTae <lsnwan@naver.com>
2023-04-19 11:39:09 +0900

    회원가입 후 처리 구현

--- frontend/src/pages/cmm/SignUpComplete.tsx
+++ frontend/src/pages/cmm/SignUpComplete.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, useEffect} from 'react';
 import {ButtonPrimary, CommonBody, Title,} from "../../components/styled/common-styled";
 import {Helmet} from "react-helmet";
 import {useSelector} from "react-redux";
@@ -10,12 +10,17 @@ type LocationType = {
   email: string;
 }
 
-const SignUpComplete: React.FC = () => {
+const SignUpComplete = () => {
+  const theme = useSelector<AppState, T.State>(state => state.themeType);
+  const navigate = useNavigate();
   const location = useLocation();
   const {email} = location.state || {email: ''};
 
-  const navigate = useNavigate();
-  const theme = useSelector<AppState, T.State>(state => state.themeType);
+  useEffect(() => {
+    if (email === '') {
+      navigate(-1);
+    }
+  }, [])
 
   return (
     <CommonBody theme={theme}>
@@ -29,7 +34,7 @@ const SignUpComplete: React.FC = () => {
         <p>가입하신 <strong>{email}</strong> 메일로 인증 메일을 발송 하였습니다.</p>
         <p>메일을 확인 후 인증을 완료해 주세요</p>
       </div>
-      
+
       <div>
         <ButtonPrimary className="block mt-3" onClick={() => navigate('/login')}>로그인 페이지로</ButtonPrimary>
       </div>
